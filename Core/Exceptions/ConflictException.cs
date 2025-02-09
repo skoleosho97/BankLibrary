f08b02c482504ad6335aa13680d8e1bc8d6209b5
skoleosho97 <samson.koleosho@smoothstack.com>
2025-02-08 23:16:41 -0500

    Made changes to how Exceptions were handled.

--- Core/Exceptions/ConflictException.cs
+++ Core/Exceptions/ConflictException.cs
@@ -1,4 +1,4 @@
-namespace Core.Exceptions.Conflict
+namespace Core.Exceptions
 {
     public class ConflictException : Exception
     {
--- Core/Exceptions/NotFound/ApplicantNotFoundException.cs
+++
@@ -1,7 +0,0 @@
-namespace Core.Exceptions.NotFound
-{
-    public class ApplicantNotFoundException : Exception
-    {
-        public ApplicantNotFoundException() : base("Applicant could not be found.") {}
-    }
-}
