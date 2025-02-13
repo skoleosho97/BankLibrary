7ba5fc6b85d252e20c91625c8d277145486fa550
skoleosho97 <samson.koleosho@smoothstack.com>
2025-02-13 02:37:28 -0500

    Added migration to add Member and Account

---
+++ Middleware/Migrations/20250213010850_AddMember_AddAccount.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Middleware.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMember_AddAccount : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Members",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    MembershipId = table.Column<string>(type: "text", nullable: false),
+                    ApplicantId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Members", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Members_Applicants_ApplicantId",
+                        column: x => x.ApplicantId,
+                        principalTable: "Applicants",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Accounts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    AccountNumber = table.Column<string>(type: "text", nullable: false),
+                    Status = table.Column<string>(type: "text", nullable: false),
+                    PrimaryAccountHolderId = table.Column<int>(type: "integer", nullable: false),
+                    Balance = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Accounts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Accounts_Members_PrimaryAccountHolderId",
+                        column: x => x.PrimaryAccountHolderId,
+                        principalTable: "Members",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Account_Holder",
+                columns: table => new
+                {
+                    AccountsId = table.Column<int>(type: "integer", nullable: false),
+                    MembersId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Account_Holder", x => new { x.AccountsId, x.MembersId });
+                    table.ForeignKey(
+                        name: "FK_Account_Holder_Accounts_AccountsId",
+                        column: x => x.AccountsId,
+                        principalTable: "Accounts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Account_Holder_Members_MembersId",
+                        column: x => x.MembersId,
+                        principalTable: "Members",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Account_Holder_MembersId",
+                table: "Account_Holder",
+                column: "MembersId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Accounts_PrimaryAccountHolderId",
+                table: "Accounts",
+                column: "PrimaryAccountHolderId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Members_ApplicantId",
+                table: "Members",
+                column: "ApplicantId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Account_Holder");
+
+            migrationBuilder.DropTable(
+                name: "Accounts");
+
+            migrationBuilder.DropTable(
+                name: "Members");
+        }
+    }
+}
