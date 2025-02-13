e20b5f29dce2f542472a0209327e06ebd87bd809
skoleosho97 <samson.koleosho@smoothstack.com>
2025-02-13 15:42:16 -0500

    Add new temporary testing for debugging purposes

--- Tests/EndpointTests/ApplicationEndpointTests.cs
+++ Tests/EndpointTests/ApplicationEndpointTests.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Json;
@@ -5,6 +6,7 @@ using System.Threading.Tasks;
 using Core.Dtos.Requests;
 using Core.Dtos.Responses;
 using FluentAssertions;
+using Tests.Utils;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -23,7 +25,7 @@ namespace Tests.EndpointTests
         }
 
         [Fact]
-        public async Task GET_POST_ReturnCreated_Apply()
+        public async Task POST_ReturnCreated_NoNewApplicantsFALSE_Apply()
         {
             CreateApplicationRequest request = new()
             {
@@ -39,7 +41,36 @@ namespace Tests.EndpointTests
 
             if (application is not null)
             {
-                output.WriteLine("Id: {0}", application.Id.ToString());
+                output.WriteLine("Type: {0}", application.ApplicationType);
+                output.WriteLine("Applicants: {0}", application.Applicants);
+                output.WriteLine("Status: {0}", application.ApplicationStatus);
+                output.WriteLine("Reasons: {0}", application.Reasons);
+                output.WriteLine("Accounts Created?: {0}", application.AccountsCreated);
+                output.WriteLine("Created Accounts: {0}", application.CreatedAccounts);
+                output.WriteLine("Members Created?: {0}", application.MembersCreated);
+                output.WriteLine("Created Members: {0}", application.CreatedMembers);
+            }
+        }
+
+        [Fact]
+        public async Task POST_ReturnCreated_NoNewApplicantsTRUE_Apply()
+        {
+            List<CreateApplicantRequest> applicant_requests = MockDataGenerator.ApplicantRequestGenerator.Generate(1);
+
+            CreateApplicationRequest request = new()
+            {
+                ApplicationType = "CHECKING",
+                NoNewApplicants = false,
+                Applicants = applicant_requests
+            };
+
+            var apply_response = await client.PostAsJsonAsync("/applications", request);
+            apply_response.StatusCode.Should().Be(HttpStatusCode.Created);
+
+            var application = await apply_response.Content.ReadFromJsonAsync<ApplyResponse>();
+
+            if (application is not null)
+            {
                 output.WriteLine("Type: {0}", application.ApplicationType);
                 output.WriteLine("Applicants: {0}", application.Applicants);
                 output.WriteLine("Status: {0}", application.ApplicationStatus);
--- Tests/Utils/MockDataGenerator.cs
+++ Tests/Utils/MockDataGenerator.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using Bogus;
+using Core.Dtos.Requests;
 using Core.Models;
 using Middleware.Data;
 
@@ -40,6 +41,26 @@ namespace Tests.Utils
                 .RuleFor(x => x.LastModified, _ => DateTime.Now)
                 .RuleFor(x => x.CreatedAt, _ => DateTime.Now);
 
+        public static readonly Faker<CreateApplicantRequest> ApplicantRequestGenerator = 
+            new Faker<CreateApplicantRequest>()
+                .RuleFor(x => x.FirstName, f => f.Name.FirstName())
+                .RuleFor(x => x.MiddleName, f => f.PickRandom(f.Name.FirstName(), ""))
+                .RuleFor(x => x.LastName, f => f.Name.LastName())
+                .RuleFor(x => x.DateOfBirth, f => f.Date.RecentDateOnly())
+                .RuleFor(x => x.Gender, f => f.PickRandom("Male", "Female"))
+                .RuleFor(x => x.Email, f => f.Internet.Email())
+                .RuleFor(x => x.Phone, f => new Randomizer().Replace("##########"))
+                .RuleFor(x => x.SocialSecurity, f => new Randomizer().Replace("########"))
+                .RuleFor(x => x.DriversLicense, new Randomizer().Replace("############"))
+                .RuleFor(x => x.Income, f => f.Random.Number(1,9)*10000)
+                .RuleFor(x => x.Address, f => f.Address.StreetAddress())
+                .RuleFor(x => x.City, f => f.Address.City())
+                .RuleFor(x => x.State, f => f.Address.State())
+                .RuleFor(x => x.Zipcode, f => f.Address.ZipCode())
+                .RuleFor(x => x.MAddress, (f, x) => x.Address)
+                .RuleFor(x => x.MCity, (f, x) => x.City)
+                .RuleFor(x => x.MState, (f, x) => x.State)
+                .RuleFor(x => x.MZipcode, (f, x) => x.Zipcode);
 
         public static void Initialize(AppDbContext context)
         {
